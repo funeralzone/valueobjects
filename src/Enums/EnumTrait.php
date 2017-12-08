b145c641c0dad08d0af7919e6b8f4e5879e14d37
Chris Harrison <c.harrison1988@gmail.com>
2017-12-08 15:31:31 +0000

    Enums!

---
+++ src/Enums/EnumTrait.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Funeralzone\ValueObjects\Enums;
+
+use Funeralzone\ValueObjects\ValueObject;
+
+trait EnumTrait
+{
+    /**
+     * @var int
+     */
+    protected $value;
+
+    /**
+     * @var null|array
+     */
+    private static $constantsCache;
+
+    /**
+     * EnumTrait constructor.
+     * @param int $value
+     */
+    public function __construct(int $value)
+    {
+        // Value must be a valid option
+        if (!in_array($value, static::constantValues())) {
+            throw new \InvalidArgumentException($value . ' is not a valid value for this enum.');
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNull(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param ValueObject $object
+     * @return bool
+     */
+    public function isSame(ValueObject $object): bool
+    {
+        return ($this->toNative() === $object->toNative());
+    }
+
+    /**
+     * @param int $native
+     * @return static
+     */
+    public static function fromNative($native)
+    {
+        if (!is_int($native)) {
+            throw new \InvalidArgumentException('Can only instantiate this object with an int.');
+        }
+
+        return new static($native);
+    }
+
+    /**
+     * @return int
+     */
+    public function toNative()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return static
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        if (!in_array($name, static::constantKeys())) {
+            throw new \RuntimeException($name . ' is an invalid value for this enum.');
+        }
+
+        return new static(constant(get_called_class() . '::' . $name));
+    }
+
+    /**
+     * @return array
+     */
+    private static function constantValues(): array
+    {
+        $constants = static::constants();
+        return array_values($constants);
+    }
+
+    /**
+     * @return array
+     */
+    private static function constantKeys(): array
+    {
+        $constants = static::constants();
+        return array_keys($constants);
+    }
+
+    /**
+     * @return array
+     */
+    private static function constants(): array
+    {
+        if (static::$constantsCache !== null) {
+            return static::$constantsCache;
+        }
+
+        $reflect = new \ReflectionClass(get_called_class());
+        return static::$constantsCache = $reflect->getConstants();
+    }
+}
---
+++ src/Enums/EnumTraitTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Funeralzone\ValueObjects\Enums;
+
+use Funeralzone\ValueObjects\ValueObject;
+use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
+
+final class EnumTraitTest extends TestCase
+{
+    public function test_construct_throws_error_when_value_is_not_defined_as_a_constant()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new _EnumTrait(10);
+    }
+
+    public function test_is_null_returns_false()
+    {
+        $test = new _EnumTrait(0);
+        $this->assertFalse($test->isNull());
+    }
+
+    public function test_is_same_returns_true_if_values_match()
+    {
+        $test1 = new _EnumTrait(0);
+        $test2 = new _EnumTrait(0);
+
+        $this->assertTrue($test1->isSame($test2));
+    }
+
+    public function test_is_same_returns_false_if_values_mismatch()
+    {
+        $test1 = new _EnumTrait(0);
+        $test2 = new _EnumTrait(1);
+
+        $this->assertFalse($test1->isSame($test2));
+    }
+
+    public function test_from_native_only_accepts_an_int()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        _EnumTrait::fromNative('1');
+
+        $this->expectException(InvalidArgumentException::class);
+        _EnumTrait::fromNative(null);
+    }
+
+    public function test_to_native_returns_an_int()
+    {
+        $test = new _EnumTrait(0);
+        $this->assertTrue(is_int($test->toNative()));
+        $this->assertEquals(0, $test->toNative());
+    }
+
+    public function test_magic_static_methods_return_objects_with_correct_value()
+    {
+        $apple = _EnumTrait::APPLE();
+        $banana = _EnumTrait::BANANA();
+        $cantaloupe = _EnumTrait::CANTALOUPE();
+
+        $this->assertEquals(0, $apple->toNative());
+        $this->assertEquals(1, $banana->toNative());
+        $this->assertEquals(2, $cantaloupe->toNative());
+    }
+}
+
+/**
+ * @method static _EnumTrait APPLE()
+ * @method static _EnumTrait BANANA()
+ * @method static _EnumTrait CANTALOUPE()
+ */
+final class _EnumTrait implements ValueObject
+{
+    use EnumTrait;
+
+    public const APPLE = 0;
+    public const BANANA = 1;
+    public const CANTALOUPE = 2;
+}
