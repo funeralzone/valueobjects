36eb45663269ceab25a859a2de74125d54dbf613
Chris Harrison <c.harrison1988@gmail.com>
2017-12-20 13:29:51 +0000

    Fixing tests

--- src/CompositeTraitTest.php
+++ src/CompositeTraitTest.php
@@ -6,34 +6,29 @@ declare(strict_types=1);
 
 namespace Funeralzone\ValueObjects;
 
+use Funeralzone\ValueObjects\TestClasses\TestValueObject;
 use PHPUnit\Framework\TestCase;
 
 final class CompositeTraitTest extends TestCase
 {
     public function test_is_null_returns_false()
     {
-        $test = new _CompositeTrait;
+        $test = new _CompositeTrait(100);
         $this->assertFalse($test->isNull());
     }
 
     public function test_is_same_returns_true_if_values_are_the_same()
     {
-        $test1 = new _CompositeTrait;
-        $test1->setTestValue(5000);
-
-        $test2 = new _CompositeTrait;
-        $test2->setTestValue(5000);
+        $test1 = new _CompositeTrait(100);
+        $test2 = new _CompositeTrait(100);
 
         $this->assertTrue($test1->isSame($test2));
     }
 
     public function test_is_same_returns_false_if_values_differ()
     {
-        $test1 = new _CompositeTrait;
-        $test1->setTestValue(5000);
-
-        $test2 = new _CompositeTrait;
-        $test2->setTestValue(700001);
+        $test1 = new _CompositeTrait(100);
+        $test2 = new _CompositeTrait(200);
 
         $this->assertFalse($test1->isSame($test2));
     }
@@ -44,7 +39,7 @@ final class CompositeTraitTest extends TestCase
             'testValue' => 100
         ];
 
-        $test = new _CompositeTrait;
+        $test = new _CompositeTrait(100);
 
         $this->assertEquals($expectedArray, $test->toNative());
     }
@@ -56,9 +51,14 @@ final class _CompositeTrait implements ValueObject
 
     private $testValue = 100;
 
-    public function setTestValue($value)
+    public function __construct(int $testValue)
     {
-        $this->testValue = $value;
+        $this->testValue = TestValueObject::fromNative($testValue);
+    }
+
+    public function getTestValue(): TestValueObject
+    {
+        return $this->testValue;
     }
 
     public static function fromNative($native)
